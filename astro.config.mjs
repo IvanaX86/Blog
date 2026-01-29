b4049713f5f4d59dc824baf48704e10ef80208f0
Ivana Walencja Zrobczynska <github@ivanax86.dev>
2026-01-29 02:05:44 +0000

    Added Licence and edited README

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+// Themes
+import pagePlugin from "@pelagornis/page";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://blog.ivanax86.dev/',
 	integrations: [
 		starlight({
 			title: 'My Docs',
+      plugins: [pagePlugin()],
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
